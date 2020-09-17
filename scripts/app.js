90f3120a69b3131ae06554df2bc97afdb4e8a2ee
Tasha <xtasherx@gmail.com>
2020-09-17 00:44:45 -0500

    fixed placeholders in contact form

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// object for emailjs parameters
+const data = {
+  service_id: "contact_service",
+  template_id: "template_vqmdehr",
+  user_id: "user_N1lY8Mc7IHZz5e2Jv6ZE1",
+  template_params: {
+    from_name: "",
+    email: "",
+    message: "",
+  },
+};
