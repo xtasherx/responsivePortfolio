f5973735ef31ce4e8a615ea914a5af0d14d1f432
Tasha <xtasherx@gmail.com>
2021-02-12 20:54:55 -0600

    set up form submission/email.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 // object for emailjs parameters
+const submit = document.querySelector("#submit-btn");
+const formName = document.querySelector(".form-name");
+const formEmail = document.querySelector(".form-email");
+const formText = document.querySelector(".form-text");
+const formConf = document.querySelector(".form-confirmation")
+let bool = false;
+
 const data = {
   service_id: "contact_service",
   template_id: "template_vqmdehr",
@@ -9,3 +16,33 @@ const data = {
     message: "",
   },
 };
+
+const toggle = () => {
+  !bool ? formConf.style.display ="none": formConf.style.display = "block";
+}
+
+toggle();
+
+submit.addEventListener("click", () => {
+  bool = true;
+
+  data.template_params.from_name = formName.value;
+  data.template_params.email = formEmail.value;
+  data.template_params.message = formText.value;
+
+  console.log(data.template_params.from_name);
+  console.log("I was clicked");
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+}).done(function() {
+    toggle();
+    formName.value = "";
+    formEmail.value = "";
+    formText.value = "";
+}).fail(function(error) {
+    alert('Oops... ' + JSON.stringify(error));
+});
+})
+
